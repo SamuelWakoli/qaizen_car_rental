61fb98fb08fa3ad7b6059b1ce0192c5d8bb3166c
samuel-wakoli <swwakoli@gmail.com>
2023-08-31 15:05:42 +0300

    improved search bar

--- lib/ui/pages/search_page.dart
+++ lib/ui/pages/search_page.dart
@@ -29,123 +29,112 @@ class _SearchPageState extends State<SearchPage> {
     return Scaffold(
       appBar: AppBar(
         centerTitle: true,
-        title: CircleAvatar(
-          radius: 22,
-          backgroundColor: Colors.white,
-          child: Image.asset('assets/ic_launcher.png'),
+        automaticallyImplyLeading: false,
+        title: Card(
+          elevation: 0.6,
+          child: TextField(
+            autofocus: true,
+            autocorrect: true,
+            focusNode: focusNode,
+            controller: searchController,
+            onChanged: (value) {
+              setState(() {});
+            },
+            onSubmitted: (value) {
+              setState(() {});
+            },
+            onTapOutside: (value) {
+              setState(() {});
+              focusNode.unfocus();
+            },
+            decoration: InputDecoration(
+                hintText: "Search",
+                icon: IconButton(
+                    onPressed: () {
+                      Navigator.pop(context);
+                    },
+                    icon: const Icon(Icons.arrow_back)),
+                suffixIcon: IconButton(
+                  tooltip: "Clear",
+                  onPressed: () {
+                    setState(() {
+                      searchController.clear();
+                    });
+                  },
+                  icon: const Icon(Icons.clear),
+                ),
+                focusedBorder: InputBorder.none,
+                enabledBorder: InputBorder.none),
+            textInputAction: TextInputAction.search,
+          ),
         ),
       ),
-      body: Padding(
-        padding: const EdgeInsets.all(8.0),
-        child: Column(
-          children: [
-            Padding(
-              padding: const EdgeInsets.only(bottom: 8.0),
-              child: TextField(
-                autofocus: true,
-                autocorrect: true,
-                focusNode: focusNode,
-                controller: searchController,
-                onChanged: (value) {
-                  setState(() {});
-                },
-                onSubmitted: (value) {
-                  setState(() {});
-                },
-                onTapOutside: (value) {
-                  setState(() {});
-                  focusNode.unfocus();
-                },
-                decoration: InputDecoration(
-                  hintText: "Search",
-                  suffixIcon: IconButton(
-                    tooltip: "Clear",
-                    onPressed: () {
-                      setState(() {
-                        searchController.clear();
-                      });
-                    },
-                    icon: const Icon(Icons.clear),
-                  ),
-                  enabledBorder: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(24.0),
-                  ),
-                  focusedBorder: OutlineInputBorder(
-                    borderSide: BorderSide(
+      body: Column(
+        children: [
+          Expanded(
+            child: StreamBuilder(
+              stream:
+                  FirebaseFirestore.instance.collection('vehicles').snapshots(),
+              builder: (context, snapshot) {
+                if (!snapshot.hasData) {
+                  return Center(
+                    child: CircularProgressIndicator(
                       color: Theme.of(context).primaryColor,
                     ),
-                    borderRadius: BorderRadius.circular(48.0),
-                  ),
-                ),
-                textInputAction: TextInputAction.search,
-              ),
-            ),
-            Expanded(
-              child: StreamBuilder(
-                stream: FirebaseFirestore.instance
-                    .collection('vehicles')
-                    .snapshots(),
-                builder: (context, snapshot) {
-                  if (!snapshot.hasData) {
-                    return Center(
-                      child: CircularProgressIndicator(
-                        color: Theme.of(context).primaryColor,
-                      ),
-                    );
-                  }
-
-                  final searchResults =
-                      snapshot.data!.docs.toList().reversed.where((document) {
-                    // Filter logic
-                    final name = document['name'].toString().toLowerCase();
-                    final searchQuery = searchController.text.toLowerCase();
-                    return name.contains(searchQuery);
-                  }).toList();
-
-                  if (searchResults.isEmpty) {
-                    return const Center(
-                      child: Text(
-                        'No results found',
-                        style: TextStyle(fontSize: 18),
-                      ),
-                    );
-                  }
-
-                  return ListView.builder(
-                    itemCount: searchResults.length,
-                    itemBuilder: (context, index) {
-                      final document = searchResults[index];
-
-                      return availableVehicleCard(
-                        context: context,
-                        id: document.id,
-                        availability: document['availability'],
-                        image: document['displayImageURL'],
-                        name: document['name'],
-                        price: document['priceDay'],
-                        onClickHire: () {
-                          serviceType = 'Self Drive';
-                          currentVehicleDocID = document.id;
-                          hire(context: context);
-                        },
-                        onClickBook: () {
-                          currentVehicleDocID = document.id;
-                          book(context: context);
-                        },
-                        onClickDetails: () {
-                          setState(() {
-                            currentVehicleDocID = document.id;
-                            details(context: context);
-                          });
-                        },
-                      );
-                    },
                   );
-                },
-              ),
+                }
+
+                final searchResults =
+                    snapshot.data!.docs.toList().reversed.where((document) {
+                  // Filter logic
+                  final name = document['name'].toString().toLowerCase();
+                  final searchQuery = searchController.text.toLowerCase();
+                  return name.contains(searchQuery);
+                }).toList();
+
+                if (searchResults.isEmpty) {
+                  return const Center(
+                    child: Text(
+                      'No results found',
+                      style: TextStyle(fontSize: 18),
+                    ),
+                  );
+                }
+
+                return ListView.builder(
+                  itemCount: searchResults.length,
+                  itemBuilder: (context, index) {
+                    final document = searchResults[index];
+
+                    return availableVehicleCard(
+                      context: context,
+                      id: document.id,
+                      availability: document['availability'],
+                      image: document['displayImageURL'],
+                      name: document['name'],
+                      price: document['priceDay'],
+                      onClickHire: () {
+                        serviceType = 'Self Drive';
+                        currentVehicleDocID = document.id;
+                        hire(context: context);
+                      },
+                      onClickBook: () {
+                        currentVehicleDocID = document.id;
+                        book(context: context);
+                      },
+                      onClickDetails: () {
+                        setState(() {
+                          currentVehicleDocID = document.id;
+                          details(context: context);
+                        });
+                      },
+                    );
+                  },
+                );
+              },
             ),
-          ],
-        ),
+          ),
+        ],
       ),
     );
   }
